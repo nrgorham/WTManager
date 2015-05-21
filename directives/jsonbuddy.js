9a5f88ebfadaf3f4eb5cb2112b70536ce709e3e5
NateG <nrgorham@gmail.com>
2015-05-21 01:24:15 -0400

    Pre-UI Bootstrap
    I'm going to try UI Bootstrap, so this is a backup if this sucks/breaks.

---
+++ directives/jsonbuddy.js
@@ -0,0 +1,22 @@
+app.directive('jsonBuddy', function() {
+    return {
+        restrict:"E",
+        replace:true,
+        scope:{
+            dude: "=",
+            test:"="
+        },
+        templateUrl:"directives/jsonbuddy.html",
+        link:function (scope, elem, attr) {
+            scope.Char2JSON = function() {
+                console.log(scope.dude);
+                scope.CharJSON = angular.toJson(scope.dude, true);
+                console.log(scope.CharJSON);
+            }
+            scope.JSON2Char = function() {
+                scope.dude = JSON.parse(scope.CharJSON);
+            }
+            
+        }
+    }
+});
